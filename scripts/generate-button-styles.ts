4ce9ee6b57c0a7c717bfbdbdc59b27f834ba194a
David Taylor <114563+tayles@users.noreply.github.com>
2026-03-02 21:46:58 -0500

    refactor: Move button-styles into docs dir

--- scripts/generate-button-styles.ts
+++ scripts/generate-button-styles.ts
@@ -11,16 +11,16 @@ import { DEFAULT_BUTTON_SIZE, generateImage, saveImage } from '../src/utils/imag
 
 // fill color button style
 const fillCtx = generateImage('fill', DEFAULT_BUTTON_SIZE, '#C24965');
-saveImage(fillCtx, 'button-styles/filled.png');
+saveImage(fillCtx, 'docs/button-styles/filled.png');
 
 // basic gradient button style
 const basicCtx = generateImage('basic', DEFAULT_BUTTON_SIZE);
-saveImage(basicCtx, 'button-styles/basic.png');
+saveImage(basicCtx, 'docs/button-styles/basic.png');
 
 // gradient + border button style
 const borderCtx = generateImage('border', DEFAULT_BUTTON_SIZE);
-saveImage(borderCtx, 'button-styles/border.png');
+saveImage(borderCtx, 'docs/button-styles/border.png');
 
 // rainbow button style
 const rainbowCtx = generateImage('rainbow', DEFAULT_BUTTON_SIZE);
-saveImage(rainbowCtx, 'button-styles/rainbow.png');
+saveImage(rainbowCtx, 'docs/button-styles/rainbow.png');
